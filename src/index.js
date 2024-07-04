81723fa0ae103f21e44cf4d1e7a7f55ef1177633
AngelesNav <angeles.navarrete@mail.udp.cl>
2024-07-04 19:47:02 -0400

    Instalar react-router-dom y corregir importaciones

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const theme = createTheme();
-
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
+  <BrowserRouter>
     <App />
-  </ThemeProvider>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
